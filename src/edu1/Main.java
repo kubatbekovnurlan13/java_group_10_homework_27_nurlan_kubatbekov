d874baa7f75a897667d2f62fcc1d2e3db4cd8a3c
Nurlan_Kubatbekov <kubatbekovnurlan13@gmail.com>
2021-12-16 16:00:22 +0600

    #4 Added method: show movie by name or partial match in name

--- src/edu1/Main.java
+++ src/edu1/Main.java
@@ -6,14 +6,17 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         showMovies();
+        getMovieByKey("asasd");
+        getMovieByKey("Good");
+        getMovieByKey("Good omens");
     }
 
     public static void showMovies() {
         try {
             Map<String, Movie> movieMap = JsonSerializer.getMovies();
-            print("Name:","Description:");
+            print("Name:", "Description:");
             for (Map.Entry<String, Movie> m : movieMap.entrySet()) {
-                print(m.getKey(),m.getValue().getDescription());
+                print(m.getKey(), m.getValue().getDescription());
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -25,5 +28,26 @@ public class Main {
         System.out.printf(format, name, desc);
     }
 
-
+    public static void getMovieByKey(String name) {
+        try {
+            Map<String, Movie> movieMap = JsonSerializer.getMovies();
+            if (movieMap.get(name) != null) {
+                print(movieMap.get(name).getName(), movieMap.get(name).getDescription());
+            } else {
+                boolean check = true;
+                for (Map.Entry<String, Movie> m : movieMap.entrySet()) {
+                    if (m.getKey().contains(name)) {
+                        print(m.getKey(), m.getValue().getDescription());
+                        check = false;
+                    }
+                }
+                if(check){
+                    System.out.println("There is no such movie!");
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
+
